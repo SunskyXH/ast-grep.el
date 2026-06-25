a996a45ad197114f06609f5abc4a2bccb5bb3908
SunskyXH <sunskyxh@gmail.com>
2026-06-25 13:57:07 +0900

    chore(release): bump to 0.5.0

--- ast-grep.el
+++ ast-grep.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025 SunskyXH
 
 ;; Author: SunskyxXH <sunskyxh@gmail.com>
-;; Version: 0.4.1
+;; Version: 0.5.0
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: tools, matching
 ;; URL: https://github.com/sunskyxh/ast-grep.el
